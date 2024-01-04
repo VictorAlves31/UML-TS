2808c8f1093dcbc69d16352be4b3545f0aeced17
VictorAlves31 <vaf.eai18@uea.edu.br>
2024-01-04 16:51:41 -0400

    criacao da classe cliente

---
+++ src/models/usuario/cliente.ts
@@ -0,0 +1,35 @@
+class Cliente extends Usuario{
+    nome: string;
+    renach: Number
+    renavam: Number;
+
+    constructor(cpf:string){
+        super()
+        this.nome='';
+        this.renach=0;
+        this.renavam=0
+
+    }
+    
+    getAgendamentos(user:Cliente){
+        // const response = this.repositoryAgendamentos.findMany(user)
+        //return response
+
+    }
+    getRealizarAgendamentos(formularioAgendamento:Agendamento){
+        //const response = this.repositoryAgendamento.create(formularioAgendamento)
+        //return response
+
+    }
+    getPontos(user:Cliente){
+        //const pontos = Renach.getPontos(user)
+        //return pontos
+
+    }
+    getSituacao(){
+        //const situ = Renach.getSituacao(user)
+        //return situ
+
+    }
+    
+}
